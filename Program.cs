6355e5ad9da648c5d54000e0a2a3ba15e835938f
chandana03 <chandanareshma01@gmail.com>
2022-03-06 20:52:40 +0530

    fibonacci

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace logicalprograms
+{
+    class program
+    {
+        static void Main(string[] args)
+        {
+             fibonacci.fibonaacciseries();
+            //couponnumber.coupon_number();
+           // primenumber.prime_number();
+           //reversenumber.reverse_number();
+            //perfectnumber.checkperfectnumber();
+        }
+    }
+}
