1a86f81ad72d6c654db46f2128ad651c9a761c25
chandana03 <chandanareshma01@gmail.com>
2022-03-06 21:08:08 +0530

    reverse a number

---
+++ reversenumber.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace logicalprograms
+{
+    public class reversenumber
+    {
+        public static void reverse_number()
+        {
+            int n, reverse = 0, rem;
+            Console.WriteLine("Enter the number");
+            n = Convert.ToInt32(Console.ReadLine());
+
+            while(n!=0)
+            {
+                rem = n % 10;
+                reverse = reverse * 10 + rem;
+                n /= 10;
+            }
+            Console.WriteLine("reverse number :" + reverse);
+        }
+    }
+}
