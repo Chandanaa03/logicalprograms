67587f804fc3318332d204b13ec18e9e3027a1d0
chandana03 <chandanareshma01@gmail.com>
2022-03-06 21:03:59 +0530

    prime number

---
+++ primenumber.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace logicalprograms
+{
+    public class primenumber
+    {
+        public static void prime_number()
+        {
+            int n, i, m = 0, flag = 0;
+            Console.Write("Enter the number: ");
+            n = Convert.ToInt32(Console.ReadLine());
+            m = n / 2;
+            for(i=2;i<=m;i++)
+            {
+                if(n%i==0)
+                {
+                    Console.Write("Number is not prime");
+                    flag = 1;
+                    break;
+
+                }
+                if(flag==0)
+                {
+                    Console.Write("Number is Prime");
+                    break;
+                }
+            }
+        }
+    }
+}
